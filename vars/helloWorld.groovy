f0dca8450ab2082707d007e7e0417f071dc01e8b
Abhimnayu kumar <52759159+veeroo2011@users.noreply.github.com>
2023-12-15 18:52:27 +0530

    removing all function and add call method

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,9 +1,2 @@
-def Greeting() {
+def call() {
   sh 'echo Hi This is Devops Team, This is shared library'
-}
-def Message() {
-  sh 'echo This is testing'
-}
-def hostname() {
-  sh 'echo hostname'
-}
